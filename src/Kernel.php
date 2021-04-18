e292b851befceda9f192b37d6abeeb1d86e617e6
Rafal Salamon <rasa@salamonrafal.pl>
2021-04-18 18:13:59 +0200

    Example API

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace ExampleAPI;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
